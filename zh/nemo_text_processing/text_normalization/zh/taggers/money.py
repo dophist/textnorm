16782b5fd0b5de4671653362b23e2642a862758d
ricky wang <108704897+rickyw36@users.noreply.github.com>
2022-07-10 18:15:34 +0800

    update number.py and money.py

--- zh/nemo_text_processing/text_normalization/zh/taggers/money.py
+++ zh/nemo_text_processing/text_normalization/zh/taggers/money.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # coding=utf-8
 import pynini
-from nemo_text_processing.text_normalization.zh.graph_utils import NEMO_SIGMA, GraphFst, insert_space
+from nemo_text_processing.text_normalization.zh.graph_utils import NEMO_SIGMA, GraphFst, insert_space,NEMO_DIGIT
 from nemo_text_processing.text_normalization.zh.utils import get_abs_path
+from nemo_text_processing.text_normalization.zh.taggers.number import NumberFst
 from pynini.lib import pynutil
 class MoneyFst(GraphFst):
     '''
@@ -10,7 +11,7 @@ class MoneyFst(GraphFst):
     '''
     def __init__(self, deterministic: bool = True, lm: bool = False):
         super().__init__(name="number", kind="classify", deterministic=deterministic)
-        currency_graph = pynini.string_file(get_abs_path("data/money/currency.tsv"))
-        self.fst = pynutil.insert("f") + currency_graph
-
+        currency_graph = pynini.invert(pynini.string_file(get_abs_path("data/money/currency.tsv")))
+        digit_graph = pynini.invert(pynini.string_file(get_abs_path("data/number/digit.tsv")))
+        self.fst = ' ' + NumberFst().final_graph + currency_graph
 
--- zh/nemo_text_processing/text_normalization/zh/taggers/number.py
+++ zh/nemo_text_processing/text_normalization/zh/taggers/number.py
@@ -5,18 +5,22 @@ from nemo_text_processing.text_normalization.zh.graph_utils import NEMO_SIGMA, N
 from nemo_text_processing.text_normalization.zh.utils import get_abs_path
 from pynini.lib import pynutil
 class NumberFst(GraphFst):
+    '''
+        1.25 --> 一点二五
+        180000 --> 十八万
+    '''
     def __init__(self, deterministic: bool = True, lm: bool = False):
         super().__init__(name="number", kind="classify", deterministic=deterministic)
         STR_TEEN = '十'
         STR_HUND = '百'
         STR_THOU = '千'
         STR_WAN = '万'
-        STR_YI = '亿'
-        non_num = (NEMO_CHAR - NEMO_DIGIT) | pynini.accep(' ') 
+        STR_YI = '亿' 
         digit_graph = pynini.invert(pynini.string_file(get_abs_path("data/number/digit.tsv")))
         zero_graph = pynini.invert(pynini.string_file(get_abs_path("data/number/zero.tsv")))
         digit_teen_graph = pynini.invert(pynini.string_file(get_abs_path("data/number/digit_teen.tsv")))
-
+        num_wl_graph = pynini.string_file(get_abs_path("data/number/whitelist.tsv"))
+        num_graph = pynini.string_file(get_abs_path("data/number/number.tsv"))
         digit_z_graph = digit_graph|zero_graph
         digit_null_graph = digit_graph|pynini.cross('0','')
         digit_teen_graph = digit_teen_graph 
@@ -41,13 +45,22 @@ class NumberFst(GraphFst):
         wan_num_graph = (
             (thou_num_graph|hund_num_graph|teen_num_graph|digit_null_graph) + pynutil.insert(STR_WAN) + (thou_num_graph|
             (pynini.cross('0','') + pynutil.insert('零') + hund_num_graph)|(pynini.cross('0','')**2 + pynutil.insert('零') + (digit_graph + pynutil.insert(STR_TEEN) + digit_null_graph))|
-            (pynini.cross('0','')**3 + pynutil.insert('零') + digit_graph))
+            (pynini.cross('0','')**3 + pynutil.insert('零') + digit_graph)|(pynini.cross('0','')**4))
         )
         long_num_graph = (
             pynini.closure(digit_z_graph,9)
         )
+        
         graph = hund_num_graph | thou_num_graph | teen_num_graph | digit_z_graph | wan_num_graph | long_num_graph
-        self.fst = non_num + graph + non_num
+        dem_num_graph = (
+            graph + pynini.cross('.','点') + pynini.closure(digit_z_graph,1) 
+        ).optimize()
+        frac_num_graph = (
+            graph + pynini.cross('/','分之') + graph
+        ).optimize()
+        final_graph = graph | dem_num_graph | frac_num_graph
+        self.final_graph = final_graph.optimize()
+        self.fst = ' ' + final_graph + ' '
         self.fst = self.fst.optimize()
         
         
